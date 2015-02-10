cda884eb403f9fda0ca59ce2eb33ed07a9f50627
Stana He <stana@is-land.com.tw>
2015-02-10 19:19:21 +0800

    delete folder 'bin'

--- HareDB_HBaseClient_WebRestful/bin/src/main/resources/test/curl/BulkLoadResourceTest.sh
+++
@@ -1,3 +0,0 @@
-echo "testexecuteBulkload"
-curl -H "Content-Type: application/json" -d '{"schemaFilePath":"hdfs://host1:9000/schema.prop","dataPath":"hdfs://host1:9000/aaa.txt"}' http://localhost:8080/HareDB_HBaseClient_WebRestful/webapi/bulkload/schema/upload
-echo ""
--- HareDB_HBaseClient_WebRestful/bin/src/main/webapp/index.jsp
+++
@@ -1,5 +0,0 @@
-<html>
-<body>
-    <h2>RESTful Web Application!</h2>
-</body>
-</html>
