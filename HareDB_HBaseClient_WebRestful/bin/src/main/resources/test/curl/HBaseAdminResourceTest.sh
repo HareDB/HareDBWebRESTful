0dde99c9214e9b724e898dd30d5598a14780af17
Stana He <stana@is-land.com.tw>
2015-02-10 19:19:21 +0800

    delete folder 'bin'

--- HareDB_HBaseClient_WebRestful/bin/src/main/resources/test/curl/HBaseAdminResourceTest.sh
+++
@@ -1,6 +0,0 @@
-echo "createHTable"
-curl -H "Content-Type: application/json" -d '{"tableName":"table9909","columnFamilys":["cf"]}' http://localhost:8080/HareDB_HBaseClient_WebRestful/webapi/hbaseadmin/create
-echo ""
-echo "dropHTable"
-curl -i -H "Content-Type: application/json" -X DELETE http://localhost:8080/HareDB_HBaseClient_WebRestful/webapi/hbaseadmin/drop/table9909
-echo ""
