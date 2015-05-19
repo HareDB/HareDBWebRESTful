a3494137008348a76628f2dcb997f6c2b91ae8c0
Stana He <stana@is-land.com.tw>
2015-05-19 10:54:57 +0800

    Bugs fixed and support kerberos

--- HareDB_HBaseClient_FacadeAPI/src/main/java/com/haredb/client/facade/operator/HareQueryHareQL.java
+++ HareDB_HBaseClient_FacadeAPI/src/main/java/com/haredb/client/facade/operator/HareQueryHareQL.java
@@ -5,7 +5,6 @@ import java.util.List;
 
 import org.apache.hadoop.hive.cli.CliSessionState;
 import org.apache.hadoop.hive.conf.HiveConf;
-import org.apache.hadoop.hive.conf.HiveConf.ConfVars;
 import org.apache.hadoop.hive.metastore.api.FieldSchema;
 import org.apache.hadoop.hive.metastore.api.Schema;
 import org.apache.hadoop.hive.ql.processors.CommandProcessorResponse;
@@ -14,21 +13,16 @@ import org.apache.hadoop.hive.ql.session.SessionState;
 import com.haredb.HareQLConfiguration;
 import com.haredb.client.facade.bean.HareQLResultStatusBean;
 import com.haredb.client.facade.bean.MessageInfo;
-import com.haredb.hbase.coprocessor.rpc.QueryJobConf;
 import com.haredb.hbaseclient.core.Connection;
 import com.haredb.hive.metastore.connection.HiveMetaConnectionBean;
-import com.haredb.hive.metastore.connection.HiveMetaConnectionBean.EnumHiveMetaStoreConnectType;
 import com.haredb.hive.ql.HareDriver;
-import com.haredb.hive.ql.client.MetaDataAdapter;
 
 public class HareQueryHareQL extends HareContrivance{
-	private Connection connection;
 	private HiveMetaConnectionBean hiveMetaConnectionBean;
 	private String fileName="result.txt";
 	
 	public HareQueryHareQL(Connection connection, HiveMetaConnectionBean hiveMetaConnectionBean){
 		super(connection);
-		this.connection = connection;
 		this.hiveMetaConnectionBean = hiveMetaConnectionBean;
 	}
 	public HareQLResultStatusBean executeHareQL(String tempFilePath, String sql, int page, int limit){
@@ -101,42 +95,32 @@ public class HareQueryHareQL extends HareContrivance{
 	}
 	
 	private HiveConf getHiveConf(){
-	    HiveConf hiveConf = new HiveConf();
+		HiveConf hiveConf = new HiveConf();
+		hiveConf.addResource(this.connection.getConfig());
 
-        hiveConf.set(Connection.zookeeperHostString, this.connection.getZookeeperHost());
-        hiveConf.set(Connection.zookeeperPortString, this.connection.getZookeeperPort());
-        hiveConf.set(Connection.nameNodeHostPortString, this.connection.getNameNodeHostPort());
-        hiveConf.set(Connection.RMAddressHostPortString, this.connection.getRmAddressHostPort());
-        hiveConf.set(Connection.RMSchedulerAddressHostPortString, this.connection.getRmSchedulerAddressHostPort());
-        hiveConf.set(Connection.RMResourceTrackerAddressHostPortString, this.connection.getRmResourceTrackerAddressHostPort());
-        hiveConf.set(Connection.RMAdminAddressHostPortString, this.connection.getRmAdminAddressHostPort());
-        hiveConf.set(Connection.RMMRframeworkname, "yarn");
-        hiveConf.set(Connection.MRJobhistoryAddress, this.connection.getMrJobhistoryAddress());
-        hiveConf.set(Connection.YarnNodemanagerAuxServices, this.connection.getYarnNodeManagerAuxServices());
-        hiveConf.set(MetaDataAdapter.HIVEDBNAME, "default");
-        
-        if(this.hiveMetaConnectionBean.getHiveConnType().equals(EnumHiveMetaStoreConnectType.LOCAL)){
-	    	hiveConf.set(MetaDataAdapter.CONNECTIONURL, hiveMetaConnectionBean.getMetaStoreConnectURL());
-			hiveConf.set(MetaDataAdapter.CONNECTIONDRIVERNAME, hiveMetaConnectionBean.getMetaStoreConnectDriver());
-			hiveConf.set(MetaDataAdapter.CONNECTIONUSERNAME,	hiveMetaConnectionBean.getMetaStoreConnectUserName());	
-			hiveConf.set(MetaDataAdapter.CONNECTIONPASSWORD,	hiveMetaConnectionBean.getMetaStoreConnectPassword());
-        }else if(this.hiveMetaConnectionBean.getHiveConnType().equals(EnumHiveMetaStoreConnectType.REMOTE)){
-    		hiveConf.setVar(HiveConf.ConfVars.METASTOREURIS, hiveMetaConnectionBean.getMetaUris());
-			hiveConf.setBoolVar(ConfVars.METASTORE_EXECUTE_SET_UGI, false);
-        }else{
-        	hiveConf.setBoolean(HiveConf.ConfVars.HIVE_WAREHOUSE_SUBDIR_INHERIT_PERMS.varname, true);
-			hiveConf.set("hive.metastore.authorization.storage.checks", "false");			
-			hiveConf.set("hive.metastore.warehouse.dir", hiveMetaConnectionBean.getEmbedPath() + "/hive/warehouse/");			
-			hiveConf.set("hive.metastore.warehouse.dir", hiveMetaConnectionBean.getEmbedPath() + "/hive/warehouse/");
-			hiveConf.set("hive.exec.local.scratchdir", hiveMetaConnectionBean.getEmbedPath() +  "/hive/tmp/hive");
-			hiveConf.set("hive.querylog.location", hiveMetaConnectionBean.getEmbedPath() + "/hive/tmp/user/");
-			hiveConf.set("hive.log.dir", hiveMetaConnectionBean.getEmbedPath() + "/hive/tmp/user/");
-        }
-       
-        hiveConf.set(QueryJobConf.hbaseRpcTimeOut, "9999999");
-        hiveConf.set(QueryJobConf.hbaseClientRetryNumber, "1");
-        hiveConf.set(HareQLConfiguration.SECONDRESULTBEANSPLITSIZE, "500");
-        hiveConf.set(HareQLConfiguration.FIRSTRESULTBEANSPLITSIZE, "1000");
-        return hiveConf;
+		hiveConf.set("hive.dbname", "default");
+		if (this.hiveMetaConnectionBean.getHiveConnType().equals(HiveMetaConnectionBean.EnumHiveMetaStoreConnectType.LOCAL)) {
+			hiveConf.set("javax.jdo.option.ConnectionURL", this.hiveMetaConnectionBean.getMetaStoreConnectURL());
+			hiveConf.set("javax.jdo.option.ConnectionDriverName", this.hiveMetaConnectionBean.getMetaStoreConnectDriver());
+			hiveConf.set("javax.jdo.option.ConnectionUserName", this.hiveMetaConnectionBean.getMetaStoreConnectUserName());
+			hiveConf.set("javax.jdo.option.ConnectionPassword", this.hiveMetaConnectionBean.getMetaStoreConnectPassword());
+		} else if (this.hiveMetaConnectionBean.getHiveConnType().equals(HiveMetaConnectionBean.EnumHiveMetaStoreConnectType.REMOTE)) {
+			hiveConf.setVar(HiveConf.ConfVars.METASTOREURIS, this.hiveMetaConnectionBean.getMetaUris());
+			hiveConf.setBoolVar(HiveConf.ConfVars.METASTORE_EXECUTE_SET_UGI, false);
+		} else {
+			hiveConf.setBoolean(HiveConf.ConfVars.HIVE_WAREHOUSE_SUBDIR_INHERIT_PERMS.varname, true);
+			hiveConf.set("hive.metastore.authorization.storage.checks", "false");
+			hiveConf.set("hive.metastore.warehouse.dir", this.hiveMetaConnectionBean.getEmbedPath() + "/hive/warehouse/");
+			hiveConf.set("hive.metastore.warehouse.dir", this.hiveMetaConnectionBean.getEmbedPath() + "/hive/warehouse/");
+			hiveConf.set("hive.exec.local.scratchdir", this.hiveMetaConnectionBean.getEmbedPath() + "/hive/tmp/hive");
+			hiveConf.set("hive.querylog.location", this.hiveMetaConnectionBean.getEmbedPath() + "/hive/tmp/user/");
+			hiveConf.set("hive.log.dir", this.hiveMetaConnectionBean.getEmbedPath() + "/hive/tmp/user/");
+		}
+
+		hiveConf.set("hbase.rpc.timeout", "9999999");
+		hiveConf.set("hbase.client.retries.number", "1");
+		hiveConf.set(HareQLConfiguration.SECONDRESULTBEANSPLITSIZE, "500");
+		hiveConf.set(HareQLConfiguration.FIRSTRESULTBEANSPLITSIZE, "1000");
+		return hiveConf;
 	}
 }
