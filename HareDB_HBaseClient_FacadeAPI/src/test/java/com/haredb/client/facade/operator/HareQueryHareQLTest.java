60fad66ea60db2964caca0681f7a1b87fdef1eaa
Stana He <stana@is-land.com.tw>
2015-07-17 18:18:47 +0800

    edit test code

--- HareDB_HBaseClient_FacadeAPI/src/test/java/com/haredb/client/facade/operator/HareQueryHareQLTest.java
+++ HareDB_HBaseClient_FacadeAPI/src/test/java/com/haredb/client/facade/operator/HareQueryHareQLTest.java
@@ -1,5 +1,7 @@
 package com.haredb.client.facade.operator;
 
+
+import org.junit.Before;
 import org.junit.Test;
 
 import com.haredb.client.facade.bean.HareQLResultStatusBean;
@@ -9,44 +11,56 @@ import com.haredb.hive.metastore.connection.HiveMetaConnectionBean.EnumHiveMetaS
 
 public class HareQueryHareQLTest {
 
-	@Test
-	public void testexecuteHareQL(){
-		
-		Connection connection = this.getConnection();
-		HiveMetaConnectionBean hiveMetaConnectionBean = this.getHiveMetaConnectionBean();
-		
-
-		HareQueryHareQL hareQL = new HareQueryHareQL(connection, hiveMetaConnectionBean);
-		String sql = "SELECT * FROM table81";
-		int page = 10;
-		int limit = 10;
-		HareQLResultStatusBean status = hareQL.executeHareQL(null, sql, page, limit);
-		System.out.println(status.getResponseTime());
-		System.out.println(status.getRowSize());
-		System.out.println(status.getHeads());
-		System.out.println(status.getResults());
-		
-	}
-	private Connection getConnection(){
-		Connection connection = new Connection("host1", "2181");
-		connection.setNameNodeHostPort("hdfs://host1:9000");
+	
+	private Connection connection;
+	private HiveMetaConnectionBean hiveMetaConnectionBean;
+	private HareQueryHareQL hareQL;
+	
+	private String tempFilePath="temp/stana_host_c";
+	private String sqlStatement="select * from stana_host_c";
+	
+	
+	@Before
+	public void initSetup(){
+		connection = new Connection("host1", "2181");
+		connection.setNameNodeHostPort("hdfs://host1:8020");
 		connection.setRmAddressHostPort("host1:8032");
 		connection.setRmSchedulerAddressHostPort("host1:8030");
 		connection.setRmResourceTrackerAddressHostPort("host1:8031");
 		connection.setRmAdminAddressHostPort("host1:8033");
 		connection.setYarnNodeManagerAuxServices("mapreduce_shuffle");
 		connection.setMrJobhistoryAddress("host1:10020");
-		return connection;
-	}		
-	private HiveMetaConnectionBean getHiveMetaConnectionBean(){
-		HiveMetaConnectionBean hiveMetaConnectionBean = new HiveMetaConnectionBean();
+		connection.create();
 		
-		EnumHiveMetaStoreConnectType connectionTypeEnum = EnumHiveMetaStoreConnectType.LOCAL;
-		hiveMetaConnectionBean.setHiveConnType(connectionTypeEnum);
+		hiveMetaConnectionBean = new HiveMetaConnectionBean();
+		hiveMetaConnectionBean.setHiveConnType(EnumHiveMetaStoreConnectType.LOCAL);
 		hiveMetaConnectionBean.setMetaStoreConnectDriver("com.mysql.jdbc.Driver");
-		hiveMetaConnectionBean.setMetaStoreConnectURL("jdbc:mysql://192.168.1.215:3306/metastore_db");
+		hiveMetaConnectionBean.setMetaStoreConnectURL("jdbc:mysql://192.168.1.214:3306/hare");
 		hiveMetaConnectionBean.setMetaStoreConnectUserName("root");
 		hiveMetaConnectionBean.setMetaStoreConnectPassword("123456");
-		return hiveMetaConnectionBean;
+		
+		
+		hareQL = new HareQueryHareQL(connection, hiveMetaConnectionBean);
 	}
+	
+	@Test
+	public void testexecuteHareQL(){
+		int page = 10;
+		int limit = 10;
+		HareQLResultStatusBean status;
+		try {
+			status = hareQL.executeHareQL(tempFilePath, sqlStatement, page, limit);
+			System.out.println(status.getResponseTime());
+			System.out.println(status.getRowSize());
+			System.out.println(status.getHeads());
+			System.out.println(status.getResults());
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+	}
+	
+	
 }
