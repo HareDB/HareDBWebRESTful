05192e486cd7a2b20819efd45540f34e72dd9541
Stana He <stana@is-land.com.tw>
2015-07-30 14:38:31 +0800

    fix bugs for Apache Sentry

--- HareDB_HBaseClient_FacadeAPI/src/main/java/com/haredb/client/facade/operator/HareManipulationHTable.java
+++ HareDB_HBaseClient_FacadeAPI/src/main/java/com/haredb/client/facade/operator/HareManipulationHTable.java
@@ -5,10 +5,10 @@ import com.haredb.client.facade.bean.MessageInfo;
 import com.haredb.hbaseclient.core.Connection;
 import com.haredb.hbaseclient.core.HTableAccessOperator;
 
-public class HareManipulationHTable {
-	private Connection connection;
+public class HareManipulationHTable extends HareContrivance{
 	
 	public HareManipulationHTable(Connection connection){
+		super(connection);
 		this.connection = connection;
 	}
 	
@@ -22,72 +22,67 @@ public class HareManipulationHTable {
 			String qualifierNames = dataCellBean.getQualifier();
 			String value = dataCellBean.getValue();
 		
-			HTableAccessOperator accessOperator = new HTableAccessOperator();
-			accessOperator.putRecord(this.connection, tableName, rowkey, columnFamily, qualifierNames, value);
+			HTableAccessOperator.putRecord(this.connection, tableName, rowkey, columnFamily, qualifierNames, value);
 			long stopTime = System.currentTimeMillis();
 			messageBean.setStatus(MessageInfo.SUCCESS);
 			messageBean.setResponseTime(stopTime - startTime);
 		}catch(Exception e){
 			messageBean.setStatus(MessageInfo.ERROR);
-			messageBean.setException(e.getMessage());
-			throw new RuntimeException("Exception:", e);
-		}finally{
+			messageBean.setException(printStackTrace(e));
 			return messageBean;
 		}
+		return messageBean;
 	}
 	
 	public MessageInfo htableDelete(String tableName, String rowkey, String columnFamily, String qualifier){
 		MessageInfo messageBean = new MessageInfo();
 		try{
 			long startTime = System.currentTimeMillis();
-			HTableAccessOperator accessOperator = new HTableAccessOperator();
-			accessOperator.deleteRecord(this.connection, tableName, rowkey, columnFamily, qualifier);
+			HTableAccessOperator.deleteRecord(this.connection, tableName, rowkey, columnFamily, qualifier);
 			long stopTime = System.currentTimeMillis();
 			messageBean.setStatus(MessageInfo.SUCCESS);
 			messageBean.setResponseTime(stopTime - startTime);
 		}catch(Exception e){
 			messageBean.setStatus(MessageInfo.ERROR);
-			messageBean.setException(e.getMessage());
-			throw new RuntimeException("Exception:", e);
-		}finally{
+			messageBean.setException(printStackTrace(e));
 			return messageBean;
 		}
+		return messageBean;
+		
 	}
 	
 	public MessageInfo htableDelete(String tableName, String rowkey, String columnFamily){
 		MessageInfo messageBean = new MessageInfo();
 		try{
 			long startTime = System.currentTimeMillis();
-			HTableAccessOperator accessOperator = new HTableAccessOperator();
-			accessOperator.deleteOneRowRecord(this.connection, tableName, columnFamily, rowkey);
+			HTableAccessOperator.deleteOneRowRecord(this.connection, tableName, columnFamily, rowkey);
 			long stopTime = System.currentTimeMillis();
 			messageBean.setStatus(MessageInfo.SUCCESS);
 			messageBean.setResponseTime(stopTime - startTime);
 		}catch(Exception e){
 			messageBean.setStatus(MessageInfo.ERROR);
-			messageBean.setException(e.getMessage());
-			throw new RuntimeException("Exception:", e);
-		}finally{
+			messageBean.setException(printStackTrace(e));
 			return messageBean;
 		}
+		return messageBean;
+		
 	}
 	
 	public MessageInfo htableDelete(String tableName, String rowkey){
 		MessageInfo messageBean = new MessageInfo();
 		try{
 			long startTime = System.currentTimeMillis();
-			HTableAccessOperator accessOperator = new HTableAccessOperator();
-			accessOperator.deleteRowkey(this.connection, tableName, rowkey);
+			HTableAccessOperator.deleteRowkey(this.connection, tableName, rowkey);
 			long stopTime = System.currentTimeMillis();
 			messageBean.setStatus(MessageInfo.SUCCESS);
 			messageBean.setResponseTime(stopTime -startTime);
 		}catch(Exception e){
 			messageBean.setStatus(MessageInfo.ERROR);
-			messageBean.setException(e.getMessage());
-			throw new RuntimeException("Exception:", e);
-		}finally{
+			messageBean.setException(printStackTrace(e));
 			return messageBean;
 		}
+		return messageBean;
+		
 	}
 	
 }
